0b3e5e0c1f9409f6e8fb7b8977541439dfd745be
Kyrie35 <1967034393@qq.com>
2026-07-01 13:04:16 +0800

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the Apache License, Version 2.0
-# found in the LICENSE file in the root directory of this source tree.
-# 自己添加
-
-from .scared_dataset import SCAREDRAWDataset
-from .hamlyn_dataset import  HamlynDataset
-
